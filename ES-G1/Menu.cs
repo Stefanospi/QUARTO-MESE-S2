3f0e4d1a991ca6f69f7e397a652fa4d6530729f8
Stefanospi <sspitaleri26@gmail.com>
2024-06-24 16:14:31 +0200

    push

---
+++ ES-G1/Menu.cs
@@ -0,0 +1,18 @@
+﻿using ES_G1.Models;
+
+namespace ES_G1
+{
+    public class Menu
+    {
+        public static List<MenuModel> MenuItems { get; } = new List<MenuModel>
+        {
+            new MenuModel {name="Coca Cola", price = 2.50m},
+            new MenuModel {name="Pepsi", price = 2.50m},
+            new MenuModel {name="Sprite", price = 2.50m},
+            new MenuModel {name="Fanta", price = 2.50m},
+            new MenuModel {name="Margherita", price = 4.50m},
+            new MenuModel {name="4 formaggi", price = 6.50m},
+            new MenuModel {name="Pizza fritta", price = 5.50m}
+        };
+    }
+}
---
+++ ES-G1/Models/MenuModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ES_G1.Models
+{
+    public class MenuModel
+    {
+        public required string? name {  get; set; }
+        public required decimal? price { get; set; }
+
+    }
+    
+}
