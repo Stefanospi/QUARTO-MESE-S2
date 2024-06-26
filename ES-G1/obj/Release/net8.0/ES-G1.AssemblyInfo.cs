9620a88da956123c15ce55a0f83ca66943cbcb92
Stefanospi <sspitaleri26@gmail.com>
2024-06-26 15:30:43 +0200

    ES-G1

--- ES-G1/obj/Release/net8.0/ES-G1.AssemblyInfo.cs
+++ ES-G1/obj/Release/net8.0/ES-G1.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Esercizio 1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ES-G1")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ff0ab0d8a2f47211ee2cc7dd8a2025da7faddaf")]
-[assembly: System.Reflection.AssemblyProductAttribute("Esercizio 1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Esercizio 1")]
+[assembly: System.Reflection.AssemblyProductAttribute("ES-G1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ES-G1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generato dalla classe WriteCodeFragment di MSBuild.
--- ES-G1/obj/Release/net8.0/ES-G1.RazorAssemblyInfo.cs
+++ ES-G1/obj/Release/net8.0/ES-G1.RazorAssemblyInfo.cs
@@ -11,13 +11,8 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Esercizio 1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ff0ab0d8a2f47211ee2cc7dd8a2025da7faddaf")]
-[assembly: System.Reflection.AssemblyProductAttribute("Esercizio 1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Esercizio 1")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ProvideApplicationPartFactoryAttribute("Microsoft.AspNetCore.Mvc.ApplicationParts.ConsolidatedAssemblyApplicationPartFact" +
+    "ory, Microsoft.AspNetCore.Mvc.Razor")]
 
 // Generato dalla classe WriteCodeFragment di MSBuild.
 
